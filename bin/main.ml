f79807bf41d7bc963e54727bc8a0c9b13f66c6a5
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2025-09-28 23:17:56 +0100

    better error handling

--- bin/main.ml
+++ bin/main.ml
@@ -4,9 +4,11 @@ let rec main () =
   print_endline "Write a λ-Calculus expression to be reduced: ";
   print_string "> ";
   let i = read_line () in
-  let parsed = Parse.parse i in
-  let reduced = Eval.eval parsed in
-  Printf.printf ">> %s\n" (Encode.to_string reduced);
-  main ()
+  match  Parse.parse i with
+  | Some parsed ->
+    let reduced = Eval.eval parsed in
+    Printf.printf ">> %s\n" (Encode.to_string reduced);
+    main ()
+  | _ -> main ()
 
 let () = main ()
--- lib/parse.ml
+++ lib/parse.ml
@@ -1,7 +1,16 @@
 let parse s =
-  let lexbuf = Lexing.from_string s in
-  let ast = Parser.prog Lexer.read lexbuf in
-  ast
+  try
+    let lexbuf = Lexing.from_string s in
+    try
+      Some(Parser.prog Lexer.read lexbuf)
+      with
+      | Parsing.Parse_error ->
+        prerr_endline ">> Syntax error!";
+        None
+  with
+  | _ ->
+    prerr_endline ">> Lexing Error!";
+    None
 
 let parse_from_file file_path =
   let read_lines file_path =
